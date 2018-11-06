f113b8d9705206daf7a91ac3068af44c473b17b8
libiao <975663670@qq.com>
2018-11-06 18:32:39 +0800

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    {{msg}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  data() {
+    return {
+      msg: "Welcome to Your Vue.js Apps"
+    };
+  },
+  created() {
+    
+    var instance = this.ajax.create({
+      headers: { "content-type": "application/x-www-form-urlencoded" }
+    });
+    instance.post('/api/list', this.qs.stringify({name:'txgz'})).then(res => res.data);
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
